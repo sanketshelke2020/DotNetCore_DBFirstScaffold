9ca8c15e61523d0e11c7a3be42110f8c856b22b5
sanketshelke2020 <sanketshelke1317@gmail.com>
2025-07-06 12:42:20 +0530

    Initial Commit

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core_DBFirst.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public int? Age { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? City { get; set; }
+
+    public string? State { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
